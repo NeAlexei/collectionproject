3c2f009592eefe283a1e7a2b2b57cb2ce2d74092
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-15 15:41:14 +0300

    CommiT

--- src/main/java/com/kinomo/Runner.java
+++ src/main/java/com/kinomo/Runner.java
@@ -3,6 +3,8 @@ package com.kinomo;
 import com.kinomo.dao.DAO;
 import com.kinomo.dao.DAOFactory;
 import com.kinomo.dao.DAOFactory.DAOKeys;
+import com.kinomo.dao.
+import com.kinomo.model.User;
 
 public class Runner {
 
